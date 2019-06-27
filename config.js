f7a75b2245e50a647ae42a94e5a2d9072a924d41
CorasanDev <henrypl360@gmail.com>
2019-06-26 21:14:07 -0400

    change api key

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const OWM_API = '55e371b73eb5f5b1da3c34e8d7513c79'
-export const API_URL = 'https://api.openweathermap.org/data/2.5'
+export const OWM_API = '69daa07f30fe7d0e83f3ed8651277166'
+export const API_URL = 'https://api.openweathermap.org/data/2.5/weather'
