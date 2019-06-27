fa18a7affb60b8527dc69bd598b6aae8d359c563
CorasanDev <henrypl360@gmail.com>
2019-06-26 23:21:10 -0400

    toggle units between ºF and ºC

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -1,8 +1,10 @@
 // @flow
 import React, { useEffect, useState } from 'react'
-import { View, Text } from 'react-native'
+import { View, StyleSheet, TouchableOpacity } from 'react-native'
+import Text from './Text'
 import { API_URL, OWM_API } from '../../config'
 import { getWeatherByLocation } from '../api'
+import { fahrenheitToCelsius, celsiusToFahrenheit } from '../utilities'
 
 type Props = {
   long: number,
@@ -11,10 +13,21 @@ type Props = {
 
 function CurrentWeather({ long, lat }: Props) {
   const [temp, setTemp] = useState('')
+  const [isFahrenheit, setIsFahrenheit] = useState(true)
 
   const getWeather = async () => {
-    const { main } = await getWeatherByLocation(lat, long)
-    setTemp(main.temp)
+    const { main } = await getWeatherByLocation({lat, long}, )
+    setTemp(main.temp.toFixed(0))
+  }
+
+  const toggleUnit = () => {
+    if (isFahrenheit) {
+      setTemp(fahrenheitToCelsius(temp))
+      setIsFahrenheit(false)
+    } else {
+      setTemp(celsiusToFahrenheit(temp))
+      setIsFahrenheit(true)
+    }
   }
 
   useEffect(() => {
@@ -24,10 +37,54 @@ function CurrentWeather({ long, lat }: Props) {
   }, [long, lat])
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>{temp}</Text>
+    <View style={styles.container}>
+      <View style={{ flexDirection: 'row' }}>
+        <Text style={styles.temp}>{temp}</Text>
+        <View style={styles.btnContainers}>
+          <TouchableOpacity
+            onPress={toggleUnit}
+            disabled={isFahrenheit}
+            style={{ opacity: isFahrenheit ? 0.4 : 1 }}
+          >
+            <Text style={styles.degreeText}>ºF</Text>
+          </TouchableOpacity>
+          <View style={styles.line} />
+          <TouchableOpacity
+            onPress={toggleUnit}
+            disabled={!isFahrenheit}
+            style={{ opacity: !isFahrenheit ? 0.4 : 1 }}
+          >
+            <Text style={styles.degreeText}>ºC</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+  temp: {
+    fontSize: 38,
+  },
+  degreeText: {
+    fontSize: 20
+  },
+  btnContainers: {
+    flexDirection: 'row',
+    marginLeft: 14,
+    alignItems: 'center',
+    height: 30,
+  },
+  line: {
+    height: '75%',
+    width: 1,
+    backgroundColor: 'black',
+    marginLeft: 10,
+    marginRight: 8
+  }
+})
 
 export default CurrentWeather
