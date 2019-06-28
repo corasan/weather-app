812248c492e98d49c0b313a4612b4616aae3bcec
CorasanDev <henrypl360@gmail.com>
2019-06-27 23:27:24 -0400

    display city

--- src/components/Weather/WeatherLocation.js
+++ src/components/Weather/WeatherLocation.js
@@ -1,10 +1,18 @@
+// @flow
 import React, { useState, useEffect } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
-function WeatherLocation() {
+type Props = {
+  city: string,
+}
+
+function WeatherLocation({ city }: Props) {
+  const currentDate = new Date()
+  const day = new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(currentDate)
+
   return (
     <View>
-      <Text>City</Text>
+      <Text>{city}</Text>
     </View>
   )
 }
