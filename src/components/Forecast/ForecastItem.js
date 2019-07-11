73166047476d46f56903d85c224d2f39fa015509
Henry Paulino <henrypl360@gmail.com>
2019-07-10 23:49:00 -0400

    add unit to forecast

--- src/components/Forecast/ForecastItem.js
+++ src/components/Forecast/ForecastItem.js
@@ -13,12 +13,13 @@ function ForecastItem({ main, dt_txt, weather }: ForecastType): React$Element<an
   const uri = `https://openweathermap.org/img/wn/${icon}@2x.png`
 
   const temp = isFahrenheit ? temp_max : fahrenheitToCelsius(temp_max)
+  const unit =isFahrenheit ? 'F' : 'C'
 
   return (
     <View style={{ marginHorizontal: 10, alignItems: 'center' }}>
       <Image source={{ uri }} style={{ height: 40, width: 40 }} />
       <Text style={{ fontSize: 14 }}>{formatDate(dt_txt)}</Text>
-      <Text style={{ fontSize: 16 }}>{temp.toFixed(0)}</Text>
+      <Text style={{ fontSize: 16 }}>{`${temp.toFixed(0)} º${unit}`}</Text>
     </View>
   )
 }
