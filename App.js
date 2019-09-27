53a8a480841732be7a50637f18d9b93b4f35eb52
Henry <henrypl360@gmail.com>
2019-09-27 01:26:46 -0400

    get codepush updates in production only

--- App.js
+++ App.js
@@ -10,100 +10,110 @@ import Context from './src/context'
 import { fahrenheitToCelsius } from './src/utils'
 
 function App() {
-  const [permission, setPermission] = useState(false)
-  const [longitude, setLongitude] = useState(0)
-  const [latitude, setLatitude] = useState(0)
-  const [city, setCity] = useState('')
-  const [weather, setWeather] = useState({})
-  const [forecast, setForecast] = useState([])
-  const [isFahrenheit, setIsFahrenheit] = useState(true)
-  const [temp, setTemp] = useState(0)
-  const [details, setDetails] = useState(null)
-  const [todayMinMax, setTodayMinMax] = useState()
+	const [permission, setPermission] = useState(false)
+	const [longitude, setLongitude] = useState(0)
+	const [latitude, setLatitude] = useState(0)
+	const [city, setCity] = useState('')
+	const [weather, setWeather] = useState({})
+	const [forecast, setForecast] = useState([])
+	const [isFahrenheit, setIsFahrenheit] = useState(true)
+	const [temp, setTemp] = useState(0)
+	const [details, setDetails] = useState(null)
+	const [todayMinMax, setTodayMinMax] = useState()
 
-  useEffect(() => {
-    const permissionUpdate = RNLocation.subscribeToPermissionUpdates(handlePermissionUpdate)
-    admob()
-    checkPermission()
+	useEffect(() => {
+		const permissionUpdate = RNLocation.subscribeToPermissionUpdates(
+			handlePermissionUpdate
+		)
+		admob()
+		checkPermission()
 
-    return function cleanup() {
-      permissionUpdate()
-    }
-  }, [])
+		return function cleanup() {
+			permissionUpdate()
+		}
+	}, [])
 
-  const handlePermissionUpdate = (authorization: string) => {
-    if (authorization === 'authorizedWhenInUse') {
-      setPermission(true)
-    } else {
-      setPermission(false)
-    }
-  }
+	const handlePermissionUpdate = (authorization: string) => {
+		if (authorization === 'authorizedWhenInUse') {
+			setPermission(true)
+		} else {
+			setPermission(false)
+		}
+	}
 
-  useEffect(() => {
-    if (!permission) {
-      requestPermission()
-    } else {
-      setCurrentLocation()
-    }
-  }, [permission])
+	useEffect(() => {
+		if (!permission) {
+			requestPermission()
+		} else {
+			setCurrentLocation()
+		}
+	}, [permission])
 
-  const checkPermission = async () => {
-    const result = await RNLocation.checkPermission({ ios: 'whenInUse' })
-    setPermission(result)
-  }
+	const checkPermission = async () => {
+		const result = await RNLocation.checkPermission({ ios: 'whenInUse' })
+		setPermission(result)
+	}
 
-  const requestPermission = async () => {
-    const result = await RNLocation.requestPermission({ ios: 'whenInUse' });
-    setPermission(result)
-  }
+	const requestPermission = async () => {
+		const result = await RNLocation.requestPermission({ ios: 'whenInUse' })
+		setPermission(result)
+	}
 
-  const setCurrentLocation = async () => {
-    const { longitude: long, latitude: lat } = await RNLocation.getLatestLocation({ timeout: 60000 })
-    setLatitude(lat)
-    setLongitude(long)
-  }
+	const setCurrentLocation = async () => {
+		const {
+			longitude: long,
+			latitude: lat,
+		} = await RNLocation.getLatestLocation({ timeout: 60000 })
+		setLatitude(lat)
+		setLongitude(long)
+	}
 
-  useEffect(() => {
-    const getWeather = async () => {
-      const { main, name, weather: weath } = await getWeatherByLocation(latitude, longitude)
-      const { list } = await getDailyForecastByLocation(latitude, longitude)
-      setTemp(main?.temp)
-      setCity(name)
-      setWeather(weath[0])
-      setForecast(list)
-      setDetails(main)
-      Analytics.trackEvent('Weather requested', { City: name })
-    }
+	useEffect(() => {
+		const getWeather = async () => {
+			const { main, name, weather: w } = await getWeatherByLocation(
+				latitude,
+				longitude
+			)
+			const { list } = await getDailyForecastByLocation(latitude, longitude)
+			setTemp(main?.temp)
+			setCity(name)
+			setWeather(w[0])
+			setForecast(list)
+			setDetails(main)
+			Analytics.trackEvent('Weather requested', { City: name })
+		}
 
-    if (latitude && longitude) {
-      getWeather()
-    }
-  }, [latitude, longitude])
+		if (latitude && longitude) {
+			getWeather()
+		}
+	}, [latitude, longitude])
 
-  const toggleUnit = () => {
-    setIsFahrenheit(!isFahrenheit)
-  }
+	const toggleUnit = () => {
+		setIsFahrenheit(!isFahrenheit)
+	}
 
-  const context = {
-    toggleUnit,
-    temp: isFahrenheit ? temp : fahrenheitToCelsius(temp),
-    isFahrenheit,
-    details,
-    setTodayMinMax,
-    todayMinMax,
-    weather,
-    city,
-    forecast,
-    latitude,
-    longitude,
-    permission,
-  }
+	const context = {
+		toggleUnit,
+		temp: isFahrenheit ? temp : fahrenheitToCelsius(temp),
+		isFahrenheit,
+		details,
+		setTodayMinMax,
+		todayMinMax,
+		weather,
+		city,
+		forecast,
+		latitude,
+		longitude,
+		permission,
+	}
 
-  return (
-    <Context.Provider value={context}>
-      <Navigation />
-    </Context.Provider>
-  )
+	return (
+		<Context.Provider value={context}>
+			<Navigation />
+		</Context.Provider>
+	)
 }
 
-export default codePush(App)
+const CloudMate = __DEV__ ? App : codePush(App)
+
+export default CloudMate
