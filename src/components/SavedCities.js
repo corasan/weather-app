858be4bf2868d10c55c9e774c7652643b5245f3d
Henry Paulino <henrypl360@gmail.com>
2019-12-07 15:12:23 -0500

    fix save city and reset search after adding city

--- src/components/SavedCities.js
+++ src/components/SavedCities.js
@@ -2,9 +2,12 @@ import React from 'react'
 import { View, FlatList, TouchableOpacity, Text } from 'react-native'
 import { SafeAreaView } from 'react-navigation'
 import { useAppContext } from 'hooks'
+import { uniqBy } from 'lodash'
 
 export default function SavedCities({ savedCities }) {
   const { changeLocation, setCurrentLocation } = useAppContext()
+  const data = uniqBy(savedCities, 'name')
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={{ flex: 1, paddingVertical: 20, alignItems: 'center' }}>
@@ -12,7 +15,8 @@ export default function SavedCities({ savedCities }) {
         {!!savedCities && savedCities.length > 0 && (
           <FlatList
             style={{ flex: 1, width: '100%' }}
-            data={savedCities}
+            data={data}
+            extraData={data}
             renderItem={({ item }) => {
               const { coord, name } = item
               return (
@@ -28,7 +32,7 @@ export default function SavedCities({ savedCities }) {
                 onPress={setCurrentLocation}
                 style={styles.btn}
               >
-                <Text style={styles.text}>Current</Text>
+                <Text style={styles.text}>Current city</Text>
               </TouchableOpacity>
             }
             keyExtractor={(index, key) => key.toString()}
