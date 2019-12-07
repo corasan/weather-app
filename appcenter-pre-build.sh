fb3b84ad4b1ca3784d0e41f173023ddf451ea4b3
Henry Paulino <henrypl360@gmail.com>
2019-12-07 16:23:25 -0500

    update scripts

---
+++ appcenter-pre-build.sh
@@ -0,0 +1,4 @@
+echo "CREATING CONFIG FILE WITH ENVIRONMENT VARIABLES"
+echo "export const OWM_API=${OWM_API}" > ./config.js
+echo "export const API_URL=${API_URL}" > ./config.js
+echo "export const AD_UNIT=${AD_UNIT}" > ./config.js
